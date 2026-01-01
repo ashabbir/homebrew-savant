74386aeb992732a23355616ac5bd74672c0f3225
Ahmed Shabbir <ahmed.shabbir@nyu.edu>
2026-01-01 09:44:08 -0500

    savant-context 1.0.0: update formula URL + SHA

--- Formula/savant-context.rb
+++ Formula/savant-context.rb
@@ -3,8 +3,8 @@ class SavantContext < Formula
 
   desc "Context MCP server with PostgreSQL-based code indexer"
   homepage "https://github.com/ashabbir/context"
-  url "https://raw.githubusercontent.com/ashabbir/homebrew-savant/d7f3b368d11d0d29c55407f37ae2e64a246caf76/savant-context-1.0.0.tar.gz"
-  sha256 "4721b97ce7185ebd196066e495174d73226eba345422bce0cb7c4e2017e723b8"
+  url "https://raw.githubusercontent.com/ashabbir/homebrew-savant/8ddffe2e34fa3a99a5ea0c17df3987c9c0f4c939/savant-context-1.0.0.tar.gz"
+  sha256 "d466dab8db70f9f832ebcbe4c52dfe321cf10cc2acdeb7a0e0b73c3a9249177d"
   license "MIT"
 
   depends_on "python@3.10"
