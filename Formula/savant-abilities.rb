4241f0cceb40ad0d32522a3b22de492d24babfbd
Ahmed Shabbir <ahmed.shabbir@nyu.edu>
2026-01-02 20:37:24 -0500

    savant-abilities 1.0.0: add initial formula

---
+++ Formula/savant-abilities.rb
@@ -0,0 +1,32 @@
+class SavantAbilities < Formula
+  include Language::Python::Virtualenv
+
+  desc "Abilities MCP server for persona + rules resolution"
+  homepage "https://github.com/ashabbir/savant-abilities"
+  url "https://raw.githubusercontent.com/ashabbir/homebrew-savant/main/savant-abilities-1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  license "MIT"
+
+  depends_on "python@3.10"
+
+  def install
+    venv = virtualenv_create(libexec, "python3.10")
+    venv.pip_install buildpath
+    (bin/"savant-abilities").write_env_script libexec/"bin/savant-abilities", {}
+  end
+
+  test do
+    system "#{bin}/savant-abilities", "--version"
+    system "#{bin}/savant-abilities", "--help"
+  end
+
+  service do
+    run [opt_bin/"savant-abilities", "run"]
+    keep_alive true
+    environment_variables PATH: std_service_path_env
+    log_path var/"log/savant-abilities.log"
+    error_log_path var/"log/savant-abilities.error.log"
+  end
+end
+
